049daee62afd1ebcebf524e7baba15b2b770f60c
arya <88246378+aryasalem09@users.noreply.github.com>
2026-02-10 13:38:33 -0600

    make game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+//     void GameOver() {
+//         PlayerController p = new PlayerController();
+//         if(p.kills >= 30) {
+//             Debug.Log("You win!");
+//         } else {
+//             Debug.Log("You lose!");
+//         }
+//         Application.LoadLevel("Tutorial");
+        
+//     }
+ }
